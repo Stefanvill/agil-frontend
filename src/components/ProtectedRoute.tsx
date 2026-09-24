1d4fc0e91a53ac8536f93e9b63fce5b224e17808
stefanvill <Stefan@villegas.se>
2026-09-24 11:02:59 +0200

    test

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }: { children: ReactNode }) {
+  const isAuthenticated = Boolean(localStorage.getItem("accessToken"));
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
